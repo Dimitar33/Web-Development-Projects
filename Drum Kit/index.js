adfbc85bd166ec87823dbe98a944b725b1a99adf
Dimitar33 <DimPetkov777@gmail.com>
2023-05-30 21:02:17 +0100

    simon game start

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,53 +1,23 @@
 var element = document.querySelectorAll("button");
 var keyPressed = document.addEventListener("keypress", (e) => {
 
-    console.log(e.key);
-    var audio = new Audio();
-    switch (e.key) {
-        case "w":
-          audio.src = "./sounds/crash.mp3";
     
-          break;
-        case "a":
-          audio.src = "./sounds/kick-bass.mp3";
-    
-          break;
-        case "s":
-          audio.src = "./sounds/snare.mp3";
-    
-          break;
-        case "d":
-          audio.src = "./sounds/tom-1.mp3";
-    
-          break;
-        case "j":
-          audio.src = "./sounds/tom-2.mp3";
-    
-          break;
-        case "k":
-          audio.src = "./sounds/tom-3.mp3";
-    
-          break;
-        case "l":
-          audio.src = "./sounds/tom-4.mp3";
-    
-        default:
-          break;
-      }
-    
-      audio.play();
+  btnKlick(e.key)
     
 });
 
 for (let i = 0; i < element.length; i++) {
-  element[i].addEventListener("click", btnKlick);
+  element[i].addEventListener("click", function (){
+
+    btnKlick(this.classList[0]);
+  });
 }
 
-function btnKlick() {
+function btnKlick(k) {
 
   var audio = new Audio();
 
-  switch (this.classList[0]) {
+  switch (k) {
     case "w":
       audio.src = "./sounds/crash.mp3";
 
@@ -76,22 +46,19 @@ function btnKlick() {
       audio.src = "./sounds/tom-4.mp3";
 
     default:
-      break;
+      return;
   }
-
+  changeBtn(k);
   audio.play();
 }
 
-function HKeeper1 (name, age, exp , stuff){
-
-    this.name = "asd",
-    this.age = age,
-    this.exp = exp,
-    this.stuff = stuff
-    this.clean = function () {
-
-        console.log("cleaning in progress");
-    }
+function  changeBtn(key) {
+  
+  var el = document.querySelector("." + key);
+  el.classList.add("pressed");
+  setTimeout(() => {
+    el.classList.remove("pressed");
+  }, 111);
 }
 
 
